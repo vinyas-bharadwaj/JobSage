7754e629eff9f3e9f5270246d23ac8b6e80625e0
vinyas-bharadwaj <vinyasbharadwaj101@gmail.com>
2025-09-07 00:37:08 +0530

    updated settings.py

--- src/src/settings.py
+++ src/src/settings.py
@@ -150,9 +150,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 # For production
 STATIC_ROOT = BASE_DIR / 'staticfiles'
